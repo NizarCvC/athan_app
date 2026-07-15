39ebe632f84e21f13ef01887c00a686ad1daa366
Nizar Omar <nizar78787@icloud.com>
2026-07-15 16:46:46 +0300

    feat: Add localization support with Arabic and English languages

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,29 @@
+import 'package:athan_app/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
-  runApp(const MainApp());
+  runApp(const AthanApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class AthanApp extends StatelessWidget {
+  const AthanApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      locale: const Locale('en'),
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      home: const Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
